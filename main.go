7dd1f4ee18e25ad554ec4b054a92339dd1b2a9d8
William Perkola <perkola.william@gmail.com>
2014-03-14 21:46:30 +0100

    Add Mode

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 )
 
 func main() {
-  set1 := []float32{1, 2, 3, 4, 5, 6}
+  set1 := []float32{1, 1, 3, 4, 5, 6}
   fmt.Println(stat.Mean(set1))    // Gives 3.5
   fmt.Println(stat.Median(set1))  // Gives 3.5
+  fmt.Println(stat.Mode(set1))    // Gives [1]
 
-  set2 := []float32{0.94, 1.20, 1.04, 1.01, 1.14}
+  set2 := []float32{1.20, 0.94, 1.14, 1.04, 1.01, 1.14, 1.20}
   fmt.Println(stat.Mean(set2))    // Gives 1.066
   fmt.Println(stat.Median(set2))  // Gives 1.04
+  fmt.Println(stat.Mode(set2))    // Gives [1.14 1.2]
+
+  set3 := []float32{8, 7, 6, 5}
+  fmt.Println(stat.Mode(set3))    // Gives []
 }
--- statistics/stat.go
+++ statistics/stat.go
@@ -5,36 +5,58 @@ import "sort"
 type Set []float32
 
 func (a Set) Len() int {
-  return len(a)
+	return len(a)
 }
 
 func (a Set) Swap(i, j int) {
-  a[i], a[j] = a[j], a[i]
+	a[i], a[j] = a[j], a[i]
 }
 
 func (a Set) Less(i, j int) bool {
-  return a[i] < a[j]
+	return a[i] < a[j]
 }
 
 func Mean(set []float32) float32 {
-  var sum float32 = 0
-  for i := 0; i < len(set); i++ {
-    sum += set[i]
-  }
-  mean := sum/float32(len(set))
-  return mean
+	var sum float32 = 0
+	for i := 0; i < len(set); i++ {
+		sum += set[i]
+	}
+	mean := sum / float32(len(set))
+	return mean
 }
 
 func Median(set []float32) float32 {
-  sort.Sort(Set(set))
-  l := len(set)
-  var median float32
-  if l % 2 == 0 {
-    i := set[l/2]
-    j := set[l/2-1]
-    median = Mean([]float32{i, j})
-  } else {
-    median = set[len(set)/2]
+	sort.Sort(Set(set))
+	l := len(set)
+	var median float32
+	if l%2 == 0 {
+		i := set[l/2]
+		j := set[l/2-1]
+		median = Mean([]float32{i, j})
+	} else {
+		median = set[len(set)/2]
+	}
+	return median
+}
+
+func Mode(set []float32) []float32 {
+  m := make(map[float32]int)
+  for _, v := range set {
+    m[v]++
   }
-  return median
+  var mode []float32
+  var n int
+  for k, v := range m {
+    if v > 1 {
+      switch {
+        case v < n:
+        case v > n:
+          n = v
+          mode = append(mode[:0], k)
+        default:
+          mode = append(mode, k)
+      }
+    }
+  }
+	return mode
 }
